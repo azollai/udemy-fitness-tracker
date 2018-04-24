225913d3df5c819a91ede283a87687baa680acff
Andras Zollai <andrszollai@gmail.com>
2018-04-24 15:51:00 +0300

    Creating e2e tests. (check if logging in is working, and if it is possible to start and stop a training)

--- fitness-tracker/src/app/training/new-training/new-training.component.ts
+++ fitness-tracker/src/app/training/new-training/new-training.component.ts
@@ -6,16 +6,15 @@ import * as trainingActions from '../training.state';
 import { TrainingState } from '../training.state';
 
 @Component({
-  selector: 'app-new-training',
-  templateUrl: './new-training.component.html',
-  styleUrls: ['./new-training.component.scss']
-})
+             selector: 'app-new-training',
+             templateUrl: './new-training.component.html',
+             styleUrls: ['./new-training.component.scss']
+           })
 export class NewTrainingComponent implements OnInit {
   @Select(TrainingState.isLoading) isLoading$: Observable<boolean>;
   @Select(TrainingState.availableExercises) availableExercises$: Observable<boolean>;
 
-  constructor(private store: Store) {
-  }
+  constructor(private store: Store) { }
 
   ngOnInit() {
     this.store.dispatch(new trainingActions.FetchAvailableExercises());
